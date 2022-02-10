01e7476cc25332fe8d118d186dd18d53cf2f0e55
luigiquiros <luis.guerrero@unige.ch>
2022-02-10 16:48:43 +0100

    update functions annotations

--- src/inventa.py
+++ src/inventa.py
@@ -140,7 +140,7 @@ def annotations(df1, df2, df3,
         function to check the presence of annotations by feature in the combined information form gnps &/ in silico 
         
         Args:
-        df1 = clusterinfosummary # mandatory 
+        df1 = annot_gnps_df # mandatory 
         df2 = tima_results_filename
         df3 = sirius_annotations_filename
         
@@ -154,23 +154,24 @@ def annotations(df1, df2, df3,
         Returns:
         None
     """
+    #ONLY GNPS        
+    #find null values (non annotated)
+    df =df1
+    df['Annotated'] = pd.isnull(df['GNPS_INCHI_MF'])
+    #lets replace the booleans 
+    bD = {True: '0', False: '1'}
+    df['Annotated_GNPS'] = df['Annotated'].replace(bD)
+    #reduced
+    df = df[['cluster index', 'componentindex', 'Annotated_GNPS']]
+    df = df.fillna({'Annotated_GNPS':0})
 
-    if (isbd_annotations == True and sirius_annotations == True):
-        #GNPS + ISDB + SIRIUS
-        
-        #work on gnps annotations
-
-        #find null values (non annotated)
-        df1['Annotated'] = pd.isnull(df1['GNPS_INCHI_MF'])
-            #lets replace the booleans 
-        bD = {True: '0', False: '1'}
-        df1['Annotated_GNPS'] = df1['Annotated'].replace(bD)
-
+    if isbd_annotations == True:
         # work on df2 (isdb annotations)
 
         df2 = pd.merge(left=df1[['cluster index']], 
                         right=df2, 
                         how='left', left_on= 'cluster index', right_on='feature_id')
+
         #recover one value from multiple options:
         df2['score_final'] = df2['score_final'].str.split('|').str[-1].astype(float)
         df2['lib_type'] = df2['score_initialNormalized'].str.split('|').str[-1].astype(float)
@@ -190,10 +191,16 @@ def annotations(df1, df2, df3,
             df2.loc[df2['lib_type']== 'MS1_match', 'Annotated_ISDB'] = 0
         else:
             df2
+        #merge the information 
+        df = pd.merge(left=df, right=df2[['cluster index','Annotated_ISDB']], 
+                        how='left', on= 'cluster index')    
+    else:
+        df
 
-            # work on df3 (sirius annotations)
-            #get the feature id 
-
+    if sirius_annotations == True:
+        # work on df3 (sirius annotations)
+        
+        #get the feature id 
         df3['shared name'] = df3['id'].str.split('_').str[-1].astype(int)
         df3 = pd.merge(left=df1[['cluster index']], 
                     right=df3[['shared name','ConfidenceScore','ZodiacScore']], 
@@ -210,154 +217,47 @@ def annotations(df1, df2, df3,
 
         df3['Annotated_Sirius'] = df3.apply(lambda x: Sirius_annotation(x['ConfidenceScore'], x['ZodiacScore']), axis=1)
             #df3.head(2)
-
         #merge the information 
-        df = pd.merge(left=df1[['cluster index', 'componentindex', 'Annotated_GNPS']], right=df2[['cluster index','Annotated_ISDB']], 
-                        how='left', on= 'cluster index')
         df = pd.merge(left=df, right=df3[['cluster index','Annotated_Sirius']], 
                         how='left',on= 'cluster index')
+    else:
+        df
 
-        def annotations_conditions(df):
-            """ function to classify the annotations results 
-                Args:
-                df = treated and combinend table with the gnps and insilico results
-                Returns:
-                None
-            """
-            if (df['Annotated_GNPS'] == '1') | (df['Annotated_ISDB'] == '1') | (df['Annotated_Sirius'] == '1'):
-                return 1
-            else: 
-                return 0
-
-        df['annotation'] = df.apply(annotations_conditions, axis=1)
-
-    elif(isbd_annotations == True and sirius_annotations == False):
-        #GNPS + ISDB
-        #work on gnps annotations
-
-        #find null values (non annotated)
-        df1['Annotated'] = pd.isnull(df1['GNPS_INCHI_MF'])
-            #lets replace the booleans 
-        bD = {True: '0', False: '1'}
-        df1['Annotated_GNPS'] = df1['Annotated'].replace(bD)
-
-        # work on df2 (isdb annotations)
-
-        df2 = pd.merge(left=df1[['cluster index']], 
-                        right=df2, 
-                        how='left', left_on= 'cluster index', right_on='feature_id')
-        #recover one value from multiple options:
-        df2['score_final'] = df2['score_final'].str.split('|').str[-1].astype(float)
-        df2['lib_type'] = df2['score_initialNormalized'].str.split('|').str[-1].astype(float)
-        df2.drop('score_initialNormalized', axis=1, inplace=True)
-        df2['molecular_formula'] = df2['molecular_formula'].str.split('|').str[-1].astype(str)
-        
-        def score_final_isdb(final_score):
-            if final_score >= min_score_final:
-                annotated=1 #good annotation
-            else:
-                annotated=0 #'bad annotation'
-            return annotated   
-
-        df2['Annotated_ISDB'] = df2.apply(lambda x: score_final_isdb(x['score_final']), axis=1)
-                    
-        if only_ms2_annotations == True:
-            df2.loc[df2['lib_type']== 'MS1_match', 'Annotated_ISDB'] = 0
-        else:
-            df2
-            
-        df = pd.merge(left=df1[['cluster index', 'componentindex', 'Annotated_GNPS']], right=df2[['cluster index','Annotated_ISDB']], 
-                        how='left', on= 'cluster index')
-        def annotations_conditions(df):
-            """ function to classify the annotations results 
-                Args:
-                df = treated and combinend table with the gnps and insilico results
-                Returns:
-                None
-            """
-            if (df['Annotated_GNPS'] == '1') | (df['Annotated_ISDB'] == '1'):
-                return 1
-            else: 
-                return 0
-
-        df['annotation'] = df.apply(annotations_conditions, axis=1) 
-
-    elif(isbd_annotations == False and sirius_annotations == True):
-        #GNPS + SIRIUS
-        #work on gnps annotations
-
-        #find null values (non annotated)
-        df1['Annotated'] = pd.isnull(df1['GNPS_INCHI_MF'])
-            #lets replace the booleans 
-        bD = {True: '0', False: '1'}
-        df1['Annotated_GNPS'] = df1['Annotated'].replace(bD)
-
-        # work on df3 (sirius annotations)
-            #get the feature id 
-        df3['shared name'] = df3['id'].str.split('_').str[-1].astype(int)
-        df3 = pd.merge(left=df1[['cluster index']], 
-                    right=df3[['shared name','ConfidenceScore','ZodiacScore']], 
-                    how='left', left_on= 'cluster index', right_on='shared name')
-
-        df3['ConfidenceScore'] = df3['ConfidenceScore'].fillna(0)
-
-        def Sirius_annotation(ConfidenceScore, ZodiacScore):
-            if ConfidenceScore >= min_ConfidenceScore and ZodiacScore >= min_ZodiacScore:
-                annotated=1 #good annotation
-            else:
-                annotated=0 #'bad annotation'
-            return annotated
-
-        df3['Annotated_Sirius'] = df3.apply(lambda x: Sirius_annotation(x['ConfidenceScore'], x['ZodiacScore']), axis=1)
-            #df3.head(2)
-
-        df = pd.merge(left=df1[['cluster index', 'componentindex', 'Annotated_GNPS']], right=df3[['cluster index','Annotated_Sirius']], 
-                        how='left',on= 'cluster index')
-
-        def annotations_conditions(df):
-            """ function to classify the annotations results 
-                Args:
-                df = treated and combinend table with the gnps and insilico results
-                Returns:
-                None
-            """
-            if (df['Annotated_GNPS'] == '1') | (df['Annotated_Sirius'] == '1'):
-                return 1
-            else: 
-                return 0
-
-        df['annotation'] = df.apply(annotations_conditions, axis=1)
-
-    elif(isbd_annotations == False and sirius_annotations == False):
-        #ONLY GNPS
-        #work on gnps annotations
-        
-        #find null values (non annotated)
-        df1['Annotated'] = pd.isnull(df1['GNPS_INCHI_MF'])
-        #lets replace the booleans 
-        bD = {True: '0', False: '1'}
-        df1['Annotated_GNPS'] = df1['Annotated'].replace(bD)
-        #reduced
-        df = df1[['cluster index', 'componentindex', 'Annotated_GNPS']]
-        df = df.fillna({'Annotated_GNPS':0})
-
-        def annotations_gnps(df):
+    def annotations_gnps(df):
             """ function to classify the annotations results 
             Args:
             df = treated and combinend table with the gnps and insilico results
             Returns:
             None
             """
-            if (df['Annotated_GNPS'] == '1'):
-                return 1
+            if isbd_annotations == True and sirius_annotations == True:
+
+                if (df['Annotated_GNPS'] == '1') | (df['Annotated_ISDB'] == '1') | (df['Annotated_Sirius'] == '1'):
+                    return 1
+                else: 
+                    return 0
+
+            elif isbd_annotations == True and sirius_annotations == False: 
+                
+                if (df['Annotated_GNPS'] == '1') | (df['Annotated_ISDB'] == '1'):
+                    return 1
+                else: 
+                    return 0
+
+            elif isbd_annotations == False and sirius_annotations == True: 
+
+                if (df['Annotated_GNPS'] == '1') | (df['Annotated_Sirius'] == '1'):
+                    return 1
+                else: 
+                    return 0
             else: 
-                return 0
-
-        df['annotation'] = df.apply(annotations_gnps, axis=1)
-    else:
-        print("INVALID INPUT")
-
-    return df
+                if (df['Annotated_GNPS'] == '1'):
+                    return 1
+                else: 
+                    return 0
+    df['annotation'] = df.apply(annotations_gnps, axis=1)  
+ 
+    return df 
 
 def feature_component(df1,df2,df3, only_feature_specificity, min_specificity, annotation_preference):
     """ function to calculate the feature specificity and feature component, as default both columns are added. 
